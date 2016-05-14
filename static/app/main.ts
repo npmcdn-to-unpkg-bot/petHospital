b3f33592543856a328083eae12b6dc74a5eaa8f8
deikaRabbit <qwsopp@gmail.com>
2016-05-14 11:18:40 +0800

    add bootstrap css and fixed cannot refresh

--- static/app/main.ts
+++ static/app/main.ts
@@ -1,20 +1,25 @@
 import {bootstrap}  from 'angular2/platform/browser';
-import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {Component, bind} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {DashboardComponent} from './dashboard.component';
 import {PetHistoryComponent} from './petHistory.component';
 
 @Component({
     selector: 'app',
-    template : `<nav>
-    	<a [routerLink]="['Dashboard']">Dashboard</a>
-    	<a [routerLink]="['History']">History</a>
+    template : `<nav class="navbar navbar-default">
+        <div class="collapse navbar-collapse">
+    	<ul class="nav nav-pills">
+    		<li><a [routerLink]="['Dashboard']">Dashboard</a></li>
+    	    <li><a [routerLink]="['History']">History</a></li>
+	    </ul>
+	    </div>
 	</nav>
 	<router-outlet></router-outlet>`,
 	directives: [ROUTER_DIRECTIVES]
 })
 
 @RouteConfig([
+  {path:'*', name: 'Dashboard', component: DashboardComponent},
   {path:'/dashboard', name: 'Dashboard', component: DashboardComponent},
   {path:'/history', name: 'History', component: PetHistoryComponent}
 ])
@@ -22,4 +27,4 @@ import {PetHistoryComponent} from './petHistory.component';
 export class App {
 }
 
-bootstrap(App, [ROUTER_PROVIDERS]);
+bootstrap(App, [ROUTER_PROVIDERS, bind(LocationStrategy).toClass(HashLocationStrategy)]);
